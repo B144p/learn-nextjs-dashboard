398547acc8666faee9ed7d0e67f70f1969898314
B144p <gug321gug@gmail.com>
2024-06-07 00:34:18 +0700

    Learn: chapter 11 => query pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -20,6 +20,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const handleSearch = debounceCall((term: string) => {
   // const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams)
+    params.set('page', '1');
     if (term) {
       params.set('query', term)
     } else {
